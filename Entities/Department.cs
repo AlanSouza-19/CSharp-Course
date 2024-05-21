50fb301cd066194b1f5eed55865a883e4fb93f70
AlanSouza-19 <souzaalanc75@gmail.com>
2024-05-21 20:00:55 -0300

    adding files

---
+++ Entities/Department.cs
@@ -0,0 +1,11 @@
+﻿namespace CompositionCsharp;
+
+public class Department
+{
+    public string Name { get; set; }
+
+    public Department(string name = "")
+    {
+        Name = name;
+    }
+}
---
+++ StringBuilder/Entities/Post.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+namespace MyStringBuilder.Entities
+{
+    public class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+
+        public List<Comment> comments { get; set; } = new List<Comment>();
+
+        public Post()
+        {
+        }
+
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+
+        public void AddComment(Comment comment)
+        {
+            comments.Add(comment);
+        }
+
+        public void RemoveComment(Comment comment)
+        {
+            comments.Remove(comment);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            return sb.Build()
+        }
+    }
+}
