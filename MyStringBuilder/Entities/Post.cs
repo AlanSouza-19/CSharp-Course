a83a37cb1eb091ec222b5870c6675803e5360631
AlanSouza-19 <souzaalanc75@gmail.com>
2024-05-22 21:14:05 -0300

    adicionado StringBuilder à classe Post

--- MyStringBuilder/Entities/Post.cs
+++ MyStringBuilder/Entities/Post.cs
@@ -33,11 +33,20 @@ namespace MyStringBuilder.Entities
             comments.Remove(comment);
         }
 
-        //public override string ToString()
-        //{
-        //    StringBuilder sb = new StringBuilder();
-
-        //    return;
-        //}
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Title);
+            sb.Append(Likes);
+            sb.Append(" Likes - ");
+            sb.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine(Content);
+            sb.AppendLine("Comments:");
+            foreach (Comment c in comments)
+            {
+                sb.AppendLine(c.Text);
+            }
+            return sb.ToString();
+        }
     }
 }
