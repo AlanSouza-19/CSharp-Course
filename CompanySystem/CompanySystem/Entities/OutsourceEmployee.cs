34a6f850e2841586e31d1d0c7f13f1d2327c47c9
Alan Corrêa <souzaalanc75@gmail.com>
2024-06-02 15:54:31 -0300

    adicionado lógica de negócio

---
+++ CompanySystem/CompanySystem/Entities/OutsourceEmployee.cs
@@ -0,0 +1,17 @@
+﻿namespace CompanySystem.Entities;
+
+public class OutsourceEmployee : Employee
+{
+    public double AddicionalCharge { get; set; }
+
+    public OutsourceEmployee(string name, int hours, double valuePerHour, double addicionalCharge)
+        : base(name, hours, valuePerHour)
+    {
+        AddicionalCharge = addicionalCharge;
+    }
+
+    public override double Payment()
+    {
+        return base.Payment() + (1.1 * AddicionalCharge);
+    }
+}
--- CompanySystem/CompanySystem/Program.cs
+++ CompanySystem/CompanySystem/Program.cs
@@ -1,2 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+using CompanySystem.Entities;
+
+namespace CompanySystem;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        List<Employee> employees = [];
+        Console.Write("Enter the number of employees: ");
+        int numberOfEmployees = int.Parse(Console.ReadLine() ?? "");
+
+        for (int i = 1; i <= numberOfEmployees; i++)
+        {
+            Console.WriteLine($"Employee #{i} data:");
+            Console.Write("Outsourced (y/n)? ");
+            char response = char.Parse(Console.ReadLine() ?? "");
+            bool isOutsourced = response == 'y';
+
+            if (isOutsourced)
+            {
+                Console.Write("Name: ");
+                string name = Console.ReadLine() ?? "";
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine() ?? "");
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine() ?? "", CultureInfo.InvariantCulture);
+                Console.Write("Addicional charge: ");
+                double addicionalCharge = double.Parse(Console.ReadLine() ?? "", CultureInfo.InvariantCulture);
+
+                employees.Add(new OutsourceEmployee(
+                    name,
+                    hours,
+                    valuePerHour,
+                    addicionalCharge
+                ));
+
+            } else {
+                Console.Write("Name: ");
+                string name = Console.ReadLine() ?? "";
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine() ?? "");
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine() ?? "", CultureInfo.InvariantCulture);
+
+                employees.Add(new Employee(
+                    name,
+                    hours,
+                    valuePerHour
+                ));
+            }    
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("PAYMENTS:");
+            foreach (Employee employee in employees)
+            {
+                Console.WriteLine(
+                    $"{employee.Name} - " +
+                    $"R$ {employee.Payment().ToString("F2", CultureInfo.InvariantCulture)}"
+                );
+            }
+    }
+}
